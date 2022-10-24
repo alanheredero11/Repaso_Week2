5afca40e14d345824cf9837a2227f5960b68519e
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-10-24 16:18:32 +0200

    Distance Calculator

--- algebraProject/point.ts
+++ algebraProject/point.ts
@@ -27,4 +27,23 @@ export class Point{
 
         return ("(" + this.x + "," + this.y + ")")
     }
+
+    public distanceToOrigin():number{
+        
+        let resultado: number = 0; 
+
+        resultado = Math.sqrt(((this.x * this.x) - 2 * this.x * 0 + (0 * 0) + (this.y * this.y) - 2 * this.y * 0 + (0 * 0)));
+
+        return resultado;
+    }
+
+    public calculateDistance(anotherPoint:Point): number {                      // se hace tienendo en cuenta que Point interno es el origen
+
+        let resultado: number = 0;
+
+        resultado = Math.sqrt(((anotherPoint.getX() * anotherPoint.getX() - 2 * this.x * anotherPoint.getX() + (this.x * this.x)) + 
+                               (anotherPoint.getY() * anotherPoint.getY()) - 2 * this.y * anotherPoint.getY() + (this.y * this.y)));
+
+        return resultado;
+    }
 }
--- algebraProject/pointTest.ts
+++ algebraProject/pointTest.ts
@@ -6,3 +6,7 @@ console.log(myPoint.getX())
 console.log(myPoint.getY())
 
 console.log(myPoint.toString())
+
+console.log(myPoint.distanceToOrigin())
+
+console.log(myPoint.calculateDistance(new Point(39, 23)))
